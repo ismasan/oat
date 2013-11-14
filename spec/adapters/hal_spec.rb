b32039fd0c5bfe0fa8426185d713b1eeb1c1db81
Ismael Celis <ismaelct@gmail.com>
2013-11-14 21:08:59 +0000

    HAL adapter tested block-based entity

--- spec/adapters/hal_spec.rb
+++ spec/adapters/hal_spec.rb
@@ -8,14 +8,24 @@ describe Oat::Adapters::HAL do
       klass = self
 
       schema do
+        link :self, href: url_for(item.id)
+
         property :id, item.id
         properties do |attrs|
           attrs.name item.name
           attrs.age item.age
           attrs.controller_name context[:name]
         end
-        link :self, url_for(item.id)
+
         entities :friends, item.friends, klass
+
+        entity :manager, item.manager do |manager, s|
+          s.link :self, href: url_for(manager.id)
+          s.properties do |attrs|
+            attrs.name manager.name
+            attrs.age manager.age
+          end
+        end if item.manager
       end
 
       def url_for(id)
@@ -27,28 +37,38 @@ describe Oat::Adapters::HAL do
   end
 
   let(:user_class) do
-    Struct.new(:name, :age, :id, :friends)
+    Struct.new(:name, :age, :id, :friends, :manager)
   end
 
-  let(:user2) { user_class.new('Joe', 33, 2, []) }
-  let(:user1) { user_class.new('Ismael', 35, 1, [user2]) }
+  let(:friend) { user_class.new('Joe', 33, 2, []) }
+  let(:manager) { user_class.new('Jane', 29, 3, []) }
+  let(:user) { user_class.new('Ismael', 35, 1, [friend], manager) }
 
   describe '#to_hash' do
     it 'produces a HAL-compliant hash' do
-      serializer = @serializer_class.new(user1, {name: 'some_controller'})
+      serializer = @serializer_class.new(user, {name: 'some_controller'})
       serializer.to_hash.tap do |h|
-        h[:id].should == user1.id
-        h[:name].should == user1.name
-        h[:age].should == user1.age
+        # properties
+        h[:id].should == user.id
+        h[:name].should == user.name
+        h[:age].should == user.age
         h[:controller_name].should == 'some_controller'
-        h[:_links][:self].should == "http://foo.bar.com/#{user1.id}"
+        # links
+        h[:_links][:self][:href].should == "http://foo.bar.com/#{user.id}"
+        # embedded manager
+        h[:_embedded][:manager].tap do |m|
+          m[:name].should == manager.name
+          m[:age].should  == manager.age
+          m[:_links][:self][:href].should == "http://foo.bar.com/#{manager.id}"
+        end
+        # embedded friends
         h[:_embedded][:friends].size.should == 1
-        h[:_embedded][:friends][0].tap do |friend|
-          friend[:id].should == user2.id
-          friend[:name].should == user2.name
-          friend[:age].should == user2.age
-          friend[:controller_name].should == 'some_controller'
-          friend[:_links][:self].should == "http://foo.bar.com/#{user2.id}"
+        h[:_embedded][:friends][0].tap do |f|
+          f[:id].should == friend.id
+          f[:name].should == friend.name
+          f[:age].should == friend.age
+          f[:controller_name].should == 'some_controller'
+          f[:_links][:self][:href].should == "http://foo.bar.com/#{friend.id}"
         end
       end
     end
