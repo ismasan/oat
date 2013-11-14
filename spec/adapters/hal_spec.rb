2d9479a31b6b3645b28c79688cce4befd272b330
Ismael Celis <ismaelct@gmail.com>
2013-11-14 22:07:48 +0000

    Put hypermedia testing fixtures in a module

--- spec/adapters/hal_spec.rb
+++ spec/adapters/hal_spec.rb
@@ -3,50 +3,15 @@ require 'oat/adapters/hal'
 
 describe Oat::Adapters::HAL do
 
+  include Fixtures
+
   before do
-    @serializer_class = Class.new(Oat::Serializer) do
-      klass = self
-
-      schema do
-        link :self, href: url_for(item.id)
-
-        property :id, item.id
-        properties do |attrs|
-          attrs.name item.name
-          attrs.age item.age
-          attrs.controller_name context[:name]
-        end
-
-        entities :friends, item.friends, klass
-
-        entity :manager, item.manager do |manager, s|
-          s.link :self, href: url_for(manager.id)
-          s.properties do |attrs|
-            attrs.name manager.name
-            attrs.age manager.age
-          end
-        end if item.manager
-      end
-
-      def url_for(id)
-        "http://foo.bar.com/#{id}"
-      end
-    end
-
-    @serializer_class.adapter Oat::Adapters::HAL
+    serializer_class.adapter Oat::Adapters::HAL
   end
-
-  let(:user_class) do
-    Struct.new(:name, :age, :id, :friends, :manager)
-  end
-
-  let(:friend) { user_class.new('Joe', 33, 2, []) }
-  let(:manager) { user_class.new('Jane', 29, 3, []) }
-  let(:user) { user_class.new('Ismael', 35, 1, [friend], manager) }
 
   describe '#to_hash' do
     it 'produces a HAL-compliant hash' do
-      serializer = @serializer_class.new(user, {name: 'some_controller'})
+      serializer = serializer_class.new(user, {name: 'some_controller'})
       serializer.to_hash.tap do |h|
         # properties
         h[:id].should == user.id
---
+++ spec/fixtures.rb
@@ -0,0 +1,39 @@
+module Fixtures
+  
+  def self.included(base)
+    base.let(:user_class) { Struct.new(:name, :age, :id, :friends, :manager) }
+    base.let(:friend) { user_class.new('Joe', 33, 2, []) }
+    base.let(:manager) { user_class.new('Jane', 29, 3, []) }
+    base.let(:user) { user_class.new('Ismael', 35, 1, [friend], manager) }
+    base.let(:serializer_class) do
+      Class.new(Oat::Serializer) do
+        klass = self
+
+        schema do
+          link :self, href: url_for(item.id)
+
+          property :id, item.id
+          properties do |attrs|
+            attrs.name item.name
+            attrs.age item.age
+            attrs.controller_name context[:name]
+          end
+
+          entities :friends, item.friends, klass
+
+          entity :manager, item.manager do |manager, s|
+            s.link :self, href: url_for(manager.id)
+            s.properties do |attrs|
+              attrs.name manager.name
+              attrs.age manager.age
+            end
+          end if item.manager
+        end
+
+        def url_for(id)
+          "http://foo.bar.com/#{id}"
+        end
+      end
+    end
+  end
+end
