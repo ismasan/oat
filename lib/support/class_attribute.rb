8b530a7c15188c9ba7cf6896848f1edc632d4e6c
Ismael Celis <ismaelct@gmail.com>
2018-02-02 23:11:35 +0000

    Don’t need that either

--- lib/support/class_attribute.rb
+++ lib/support/class_attribute.rb
@@ -12,18 +12,6 @@ module Kernel
   end
 end
 
-class Module
-  def remove_possible_method(method)
-    if method_defined?(method) || private_method_defined?(method)
-      remove_method(method)
-    end
-  rescue NameError
-    # If the requested method is defined on a superclass or included module,
-    # method_defined? returns true but remove_method throws a NameError.
-    # Ignore this.
-  end
-end
-
 class Class
   def class_attribute(*attrs)
     attrs.each do |name|
@@ -32,13 +20,11 @@ class Class
 
           def self.#{name}=(val)
             singleton_class.class_eval do
-              remove_possible_method(:#{name})
               define_method(:#{name}) { val }
             end
 
             if singleton_class?
               class_eval do
-                remove_possible_method(:#{name})
                 def #{name}
                   defined?(@#{name}) ? @#{name} : singleton_class.#{name}
                 end
